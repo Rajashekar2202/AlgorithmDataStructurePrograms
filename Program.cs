11500e1c7955fa4ca1be3a53573eb17006f7f2a9
Rajashekar Vemula <vemula.rajashekar02@gmail.com>
2022-11-06 12:43:34 +0530

    added Anagram program

--- Program.cs
+++ Program.cs
@@ -2,45 +2,29 @@
 {
     class Anagram
     {
-        public void checkAnagrams()
+        public static void Main(string[] args)
         {
-            Console.Write("Enter first string:   ");
-            string str1 = Console.ReadLine();
-            Console.Write("\nEnter second string:   ");
-            string str2 = Console.ReadLine();
+            string str1 = "Heart";
+            string str2 = "Earth";
 
-            char[] strArr1 = str1.ToLower().ToCharArray();
-            char[] strArr2 = str2.ToLower().ToCharArray();
+            char[] chr1 = str1.ToLower().ToCharArray();
+            char[] chr2 = str2.ToLower().ToCharArray();
 
-            sorting(strArr1);
-            sorting(strArr2);
+            Array.Sort(chr1);
+            Array.Sort(chr2);
 
-            string s1 = new string(strArr1);
-            string s2 = new string(strArr2);
+            string val1 = new string(chr1);
+            Console.WriteLine(val1);
+            string val2 = new string(chr2);
+            Console.WriteLine(val2);
 
-            if (s1.Equals(s2))
+            if (val1 == val2)
             {
-                Console.WriteLine("\nString {0} and {1} are anagram of each other.", str1, str2);
+                Console.WriteLine("Anagram");
             }
             else
             {
-                Console.WriteLine("\nString {0} and {1} are not anagrams.", str1, str2);
-            }
-        }
-
-        public void sorting(char[] arr)
-        {
-            for (int i = 0; i < arr.Length; i++)
-            {
-                for (int j = 0; j < arr.Length; j++)
-                {
-                    if (arr[i] < arr[j])
-                    {
-                        char temp = arr[j];
-                        arr[j] = arr[i];
-                        arr[i] = temp;
-                    }
-                }
+                Console.WriteLine("Not Anagram");
             }
         }
     }
