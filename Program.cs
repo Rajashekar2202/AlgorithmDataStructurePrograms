a6855ef520cc1611c7941dc126a2f7764d8df940
Rajashekar Vemula <vemula.rajashekar02@gmail.com>
2022-11-06 12:52:55 +0530

    added Merge Sort program

--- Program.cs
+++ Program.cs
@@ -1,31 +1,44 @@
 ﻿namespace AlgorithmDataStructurePrograms
 {
-    class InsertionSort
-    {
-        public static void Main(string[] args)
+    class MergeSort
+    {   
+    public void MergeArray(int[] array, int left, int middle, int right)
         {
-            int[] arr = new int[] { 23, 9, 2 };
-            int n = 3, i, j, val, flag;
+            var leftArrayLength = middle - left + 1;
+            var rightArrayLength = right - middle;
+            var leftTempArray = new int[leftArrayLength];
+            var rightTempArray = new int[rightArrayLength];
 
-            for (i = 0; i < n; i++)
+            int i, j;
+
+            for (i = 0; i < leftArrayLength; ++i)
+                leftTempArray[i] = array[left + i];
+            for (j = 0; j < rightArrayLength; ++j)
+                rightTempArray[j] = array[middle + 1 + j];
+
+            i = 0;
+            j = 0;
+
+            int k = left;
+
+            while (i < leftArrayLength && j < rightArrayLength)
             {
-                val = arr[i];
-                flag = 0;
-
-                for (j = i - 1; j >= 0 && flag != 1;)
+                if (leftTempArray[i] <= rightTempArray[j])
                 {
-                    if (val < arr[j])
-                    {
-                        arr[j + 1] = arr[j];
-                        j--;
-                        arr[j + 1] = val;
-                    }
-                    else flag = 1;
+                    array[k++] = leftTempArray[i++];
+                }
+                else
+                {
+                    array[k++] = rightTempArray[j++];
                 }
             }
-            foreach (int ele in arr)
+            while (i < leftArrayLength)
             {
-                Console.WriteLine(ele);
+                array[k++] = leftTempArray[i++];
+            }
+            while (j < rightArrayLength)
+            {
+                array[k++] = rightTempArray[j++];
             }
         }
     }
